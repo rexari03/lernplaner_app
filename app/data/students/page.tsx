f02e7cbe5bd304077517f75d957fae743cb2993e
Calvin Teuber <calvin.teuber@volkswagen.de>
2024-05-31 00:00:56 +0200

    added support for adding students

--- app/data/students/page.tsx
+++ app/data/students/page.tsx
@@ -7,32 +7,40 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import {fetchAllStudents} from "@/api/studentService";
 import {StudentBrief} from "@/types/studentBrief";
 import {useRouter} from "next/navigation";
+import {Class} from "@/types/class";
+import {getAllClasses} from "@/api/classService";
+import Button from "react-bootstrap/Button";
+import AddStudentForm from "@/components/add-student-form";
 
 const StudentsPage = () => {
     const [students, setStudents] = useState<StudentBrief[]>([]);
+    const [classes, setClasses] = useState<Class[]>([]);
+
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(20);
     const [isLoading, setIsLoading] = useState(false);
+    const [showAddForm, setShowAddForm] = useState(false);
+
     const router = useRouter();
 
-    useEffect(() => {
-        const fetchStudents = async () => {
-            setIsLoading(true);
-            const users: StudentBrief[] = await fetchAllStudents();
-            setStudents(users);
-            setIsLoading(false);
-        }
+    const fetchStudents = async () => {
+        const users: StudentBrief[] = await fetchAllStudents();
+        setStudents(users);
+    }
+    const fetchClasses = async () => {
+        const classes: Class[] = await getAllClasses();
+        setClasses(classes);
+    }
+
+    useEffect( () => {
+        setIsLoading(true);
         fetchStudents();
+        fetchClasses();
+        setIsLoading(false);
     }, []);
 
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = students.slice(indexOfFirstItem, indexOfLastItem);
-
-    const totalPages = Math.ceil(students.length / itemsPerPage);
-
     const handleNext = () => {
-        if (currentPage < totalPages) {
+        if (currentPage < calculatePagination().totalPages) {
             setCurrentPage(currentPage + 1);
         } else {
             setCurrentPage(1)
@@ -43,8 +51,23 @@ const StudentsPage = () => {
         if (currentPage > 1) {
             setCurrentPage(currentPage - 1);
         } else {
-            setCurrentPage(totalPages)
+            setCurrentPage(calculatePagination().totalPages)
         }
+    };
+
+    const calculatePagination = () => {
+        const indexOfLastItem = currentPage * itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+        const currentItems = students.slice(indexOfFirstItem, indexOfLastItem);
+
+        const totalPages = Math.ceil(students.length / itemsPerPage);
+
+        return { currentItems, totalPages };
+    }
+
+    const handleCloseForm = async () => {
+        setShowAddForm(false);
+        await fetchStudents();
     };
 
     if (isLoading) {
@@ -62,43 +85,59 @@ const StudentsPage = () => {
     return (
         <div>
             <h1 className={"text-center display-4"}>Schüler</h1>
-            {students.length > 0 ? (
+            {showAddForm ? (
                 <>
-                    <Table striped bordered hover>
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Benutzername</th>
-                            <th>Name</th>
-                            <th>Nachname</th>
-                            <th>Geburtstag</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {currentItems.map((student, index) => (
-                            <tr key={index} onClick={() => router.push(`/data/profile/${student.account.username}`)}>
-                                <td>{student.id}</td>
-                                <td>{student.account.username}</td>
-                                <td>{student.account.name}</td>
-                                <td>{student.account.last_name}</td>
-                                <td>{student.account.birthday}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
-                    <div className={"d-flex justify-content-center"}>
-                        <button onClick={handlePrevious}
-                                className="btn btn-outline-secondary mx-2">
-                            <i className="bi bi-arrow-left"></i>
-                        </button>
-                        <span className={"mx-3 align-self-center"}>Page {currentPage} of {totalPages}</span>
-                        <button onClick={handleNext}
-                                className="btn btn-outline-secondary mx-2">
-                            <i className="bi bi-arrow-right"></i>
-                        </button>
-                    </div>
+                    <Button variant="secondary" onClick={handleCloseForm} className={"mb-2"}>
+                        <i className="bi bi-x-lg"></i>
+                    </Button>
+                    <AddStudentForm schoolClasses={classes}/>
                 </>
-            ) : null}
+            ) : (
+                <>
+                    <Button variant="secondary" onClick={() => setShowAddForm(true)} className={"mb-2"}>
+                        <i className="bi bi-plus-lg"></i>
+                    </Button>
+                    {students.length > 0 ? (
+                        <>
+                            <Table striped bordered hover>
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Benutzername</th>
+                                    <th>Name</th>
+                                    <th>Nachname</th>
+                                    <th>Geburtstag</th>
+                                    <th>Klasse</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {calculatePagination().currentItems.map((student, index) => (
+                                    <tr key={index} onClick={() => router.push(`/data/student/${student.account.username}`)}>
+                                        <td>{student.id}</td>
+                                        <td>{student.account.username}</td>
+                                        <td>{student.account.name}</td>
+                                        <td>{student.account.last_name}</td>
+                                        <td>{student.account.birthday}</td>
+                                        <td>{classes[student.school_class_id-1].name}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </Table>
+                            <div className={"d-flex justify-content-center"}>
+                                <button onClick={handlePrevious}
+                                        className="btn btn-outline-secondary mx-2">
+                                    <i className="bi bi-arrow-left"></i>
+                                </button>
+                                <span className={"mx-3 align-self-center"}>Page {currentPage} of {calculatePagination().totalPages}</span>
+                                <button onClick={handleNext}
+                                        className="btn btn-outline-secondary mx-2">
+                                    <i className="bi bi-arrow-right"></i>
+                                </button>
+                            </div>
+                        </>
+                    ) : null}
+                </>
+            )}
         </div>
     );
 }
