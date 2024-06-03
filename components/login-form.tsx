f540edea69812f630d1fde7f55a85cc7ce08d1a8
Calvin Teuber <calvin.teuber@volkswagen.de>
2024-06-03 16:23:20 +0200

    typefix

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -14,7 +14,7 @@ export default function Login() {
     const [emailError, setEmailError] = useState(false); // Use state for emailError
 
     // Simple email validation
-    const validateEmail = (email) => {
+    const validateEmail = (email: string) => {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
